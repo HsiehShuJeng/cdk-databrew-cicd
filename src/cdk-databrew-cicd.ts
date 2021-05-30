c6bcab488efb5b5ea80c983d8d8589febeb4ca59
scott.hsieh <fantasticSie@hotmail.com>
2021-05-30 11:40:25 +0800

    add codepipeline role

--- src/cdk-databrew-cicd.ts
+++ src/cdk-databrew-cicd.ts
@@ -1,2 +1,135 @@
-// import * as cdk from '@aws-cdk/core';
-// import * as iam from '@aws-cdk/aws-iam';
+import * as cdk from '@aws-cdk/core';
+import * as iam from '@aws-cdk/aws-iam';
+
+
+export interface InfraIamRoleProps {
+    /**
+     * The role name for the infrastructure account.
+     * 
+     * @default 'CrossAccountRepositoryContributorRole'
+     */
+    roleName?: string;
+}
+export class InfraIamRole extends cdk.Construct {
+    /**
+     * The ARN of the IAM role for the infrastructure account.
+     */
+    readonly roleArn: string;
+    constructor(scope: cdk.Construct, name: string, props: InfraIamRoleProps) {
+        super(scope, name);
+
+        const role = new iam.Role(this, 'InfraIamRole', {
+            assumedBy: new iam.AccountRootPrincipal(),
+            roleName: props.roleName ?? 'CrossAccountRepositoryContributorRole',
+            description: `The IAM role for DataBrew CICD in the infrastructure account.`
+        });
+        role.addToPolicy(new iam.PolicyStatement({
+            sid: 'CodeCommitPermissions',
+            effect: iam.Effect.ALLOW,
+            actions: [
+                'codecommit:BatchGet*',
+                'codecommit:Create*',
+                'codecommit:DeleteBranch',
+                'codecommit:Get*',
+                'codecommit:List*',
+                'codecommit:Describe*',
+                'codecommit:Put*',
+                'codecommit:Post*',
+                'codecommit:Merge*',
+                'codecommit:Test*',
+                'codecommit:Update*',
+                'codecommit:GitPull',
+                'codecommit:GitPush'],
+            resources: [
+                `arn:${cdk.Aws.PARTITION}:codecommit:${cdk.Aws.REGION}:${cdk.Aws.ACCOUNT_ID}:DataBrew-Recipes-Repo`]
+        }));
+        role.addToPolicy(new iam.PolicyStatement({
+            sid: 'CodeCommitListReposPermission',
+            effect: iam.Effect.ALLOW,
+            actions: [
+                'codecommit:ListRepositories'],
+            resources: ['*']
+        }));
+        this.roleArn = role.roleArn;
+    }
+}
+
+export interface CodePipelineIamRoleProps {
+    /**
+     * The ARN of the S3 bucket where you store your ???.
+     */
+    bucketArn: string;
+    /**
+     * The ARN of the Lambda function for the pre-production account.
+     */
+    preproductionLambdaArn: string;
+    /**
+     * The ARN of the Lambda function for the production account.
+     */
+     productionLambdaArn: string;
+    /**
+     * The role name for the CodePipeline CICD pipeline.
+     * 
+     * @default 'DataBrew-Recipe-Pipeline-Role'
+     */
+    roleName?: string;
+}
+
+export class CodePipelineIamRole extends cdk.Construct {
+    /**
+     * The ARN of the IAM role for the CodePipeline CICD pipeline.
+     */
+    readonly roleArn: string;
+    constructor(scope: cdk.Construct, name: string, props: CodePipelineIamRoleProps) {
+        super(scope, name);
+        const role = new iam.Role(this, 'CodePipelineIamRole', {
+            assumedBy: new iam.ServicePrincipal('codepipeline.amazonaws.com '),
+            roleName: props.roleName ?? 'DataBrew-Recipe-Pipeline-Role',
+            description: `The IAM role for the CodePipeline CICD pipeline in the infrastructure account.`
+        });
+        // CodeDeploy permissions
+        role.addToPolicy(new iam.PolicyStatement({
+            sid: 'CodeDeployPermissions',
+            effect: iam.Effect.ALLOW,
+            actions: [
+                'codedeploy:CreateDeployment',
+                'codedeploy:GetApplicationRevision',
+                'codedeploy:GetDeployment',
+                'codedeploy:GetDeploymentConfig',
+                'codedeploy:RegisterApplicationRevision'],
+            resources: [
+                `arn:${cdk.Aws.PARTITION}:codedeploy:${cdk.Aws.REGION}:${cdk.Aws.ACCOUNT_ID}:deploymentgroup:DataBrew-Recipe-Application*`,
+                `arn:${cdk.Aws.PARTITION}:codedeploy:${cdk.Aws.REGION}:${cdk.Aws.ACCOUNT_ID}:application:DataBrew-Recipe-Application`]
+        }));
+        // CodeCommit permissions
+        role.addToPolicy(new iam.PolicyStatement({
+            sid: 'CodeCommitPermissions',
+            effect: iam.Effect.ALLOW,
+            actions: [
+                'codecommit:GetBranch',
+                'codecommit:GetCommit',
+                'codecommit:GetUploadArchiveStatus',
+                'codecommit:UploadArchive'],
+            resources: [
+                `arn:${cdk.Aws.PARTITION}:codecommit:${cdk.Aws.REGION}:${cdk.Aws.ACCOUNT_ID}:DataBrew-Recipes-Repo`]
+        }));
+        // S3 permissions
+        role.addToPolicy(new iam.PolicyStatement({
+            sid: 'S3Permissions',
+            effect: iam.Effect.ALLOW,
+            actions: [
+                's3:GetObject',
+                's3:PutObject'],
+            resources: [props.bucketArn]
+        }));
+        // Lambda permissions
+        role.addToPolicy(new iam.PolicyStatement({
+            sid: 'LambdaPermissions',
+            effect: iam.Effect.ALLOW,
+            actions: [
+                'lambda:InvokeFunction'],
+            resources: [props.preproductionLambdaArn, props.productionLambdaArn]
+        }));
+        this.roleArn = role.roleArn;
+    }
+}
--- test/integ.test.ts
+++ test/integ.test.ts
@@ -1,12 +1,152 @@
 import { SynthUtils } from '@aws-cdk/assert';
 import '@aws-cdk/assert/jest';
 import * as cdk from '@aws-cdk/core';
+import { CodePipelineIamRole } from '../src/cdk-databrew-cicd';
 
 test('IamRole', () => {
   const app = new cdk.App();
   const infraStack = new cdk.Stack(app, 'infra-stack');
 
 
+  new CodePipelineIamRole(infraStack, 'DataBrewCodePipeline', {
+    bucketArn: 'bucketArn',
+    preproductionLambdaArn: 'preproductionArn',
+    productionLambdaArn: 'productionArn'
+  });
+
+
+
 
   expect(SynthUtils.toCloudFormation(infraStack)).toMatchSnapshot();
+  expect(SynthUtils.toCloudFormation(infraStack)).toCountResources('AWS::IAM::Role', 1);
+  expect(infraStack).toHaveResourceLike('AWS::IAM::Role', {
+    "AssumeRolePolicyDocument": {
+      "Statement": [
+        {
+          "Action": "sts:AssumeRole",
+          "Effect": "Allow",
+          "Principal": {
+            "Service": "codepipeline.amazonaws.com "
+          }
+        }
+      ],
+      "Version": "2012-10-17"
+    },
+  });
+
+  expect(SynthUtils.toCloudFormation(infraStack)).toCountResources('AWS::IAM::Policy', 1);
+  expect(infraStack).toHaveResourceLike('AWS::IAM::Policy', {
+    "PolicyDocument": {
+      "Statement": [
+        {
+          "Action": [
+            "codedeploy:CreateDeployment",
+            "codedeploy:GetApplicationRevision",
+            "codedeploy:GetDeployment",
+            "codedeploy:GetDeploymentConfig",
+            "codedeploy:RegisterApplicationRevision"
+          ],
+          "Effect": "Allow",
+          "Resource": [
+            {
+              "Fn::Join": [
+                "",
+                [
+                  "arn:",
+                  {
+                    "Ref": "AWS::Partition"
+                  },
+                  ":codedeploy:",
+                  {
+                    "Ref": "AWS::Region"
+                  },
+                  ":",
+                  {
+                    "Ref": "AWS::AccountId"
+                  },
+                  ":deploymentgroup:DataBrew-Recipe-Application*"
+                ]
+              ]
+            },
+            {
+              "Fn::Join": [
+                "",
+                [
+                  "arn:",
+                  {
+                    "Ref": "AWS::Partition"
+                  },
+                  ":codedeploy:",
+                  {
+                    "Ref": "AWS::Region"
+                  },
+                  ":",
+                  {
+                    "Ref": "AWS::AccountId"
+                  },
+                  ":application:DataBrew-Recipe-Application"
+                ]
+              ]
+            }
+          ],
+          "Sid": "CodeDeployPermissions"
+        },
+        {
+          "Action": [
+            "codecommit:GetBranch",
+            "codecommit:GetCommit",
+            "codecommit:GetUploadArchiveStatus",
+            "codecommit:UploadArchive"
+          ],
+          "Effect": "Allow",
+          "Resource": {
+            "Fn::Join": [
+              "",
+              [
+                "arn:",
+                {
+                  "Ref": "AWS::Partition"
+                },
+                ":codecommit:",
+                {
+                  "Ref": "AWS::Region"
+                },
+                ":",
+                {
+                  "Ref": "AWS::AccountId"
+                },
+                ":DataBrew-Recipes-Repo"
+              ]
+            ]
+          },
+          "Sid": "CodeCommitPermissions"
+        },
+        {
+          "Action": [
+            "s3:GetObject",
+            "s3:PutObject"
+          ],
+          "Effect": "Allow",
+          "Resource": "bucketArn",
+          "Sid": "S3Permissions"
+        },
+        {
+          "Action": "lambda:InvokeFunction",
+          "Effect": "Allow",
+          "Resource": [
+            "preproductionArn",
+            "productionArn"
+          ],
+          "Sid": "LambdaPermissions"
+        }
+      ],
+      "Version": "2012-10-17"
+    },
+    "PolicyName": "DataBrewCodePipelineCodePipelineIamRoleDefaultPolicyB989C8CA",
+    "Roles": [
+      {
+        "Ref": "DataBrewCodePipelineCodePipelineIamRole2999CE31"
+      }
+    ]
+  });
 })
