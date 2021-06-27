286646bfc75349b6d8866bce19d9c2a2e25ffb28
scott.hsieh <fantasticSie@hotmail.com>
2021-06-27 15:47:57 +0800

    merge branch

--- .projenrc.js
+++ .projenrc.js
@@ -22,7 +22,7 @@ const project = new AwsCdkConstructLibrary({
     twitter: 'fantasticHsieh',
   },
 
-  cdkVersion: '1.107.0',
+  cdkVersion: '1.110.0',
   defaultReleaseBranch: 'main',
   name: 'cdk-databrew-cicd',
   repositoryUrl: 'https://github.com/HsiehShuJeng/cdk-databrew-cicd.git',
