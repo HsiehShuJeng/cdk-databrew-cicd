b8542a958222d060dcf2aa4c7643a95a2f7e75ac
scott.hsieh <fantasticSie@hotmail.com>
2021-08-18 09:17:39 +0800

    modify projen settings

--- .projenrc.js
+++ .projenrc.js
@@ -55,7 +55,7 @@ const project = new AwsCdkConstructLibrary({
   projenUpgradeSecret: 'PROJEN_UPGRADE_SECRET',
   autoApproveOptions: {
     secret: 'GITHUB_TOKEN',
-    allowedUsernames: ['HsiehShuJeng']
+    allowedUsernames: ['HsiehShuJeng'],
   },
   depsUpgradeAutoMerge: true,
 
